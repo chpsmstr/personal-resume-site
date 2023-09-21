34bf75cda8871ea23ed2953d7a83b910a0175475
chpsmstr <prakashankkit@gmail.com>
2023-09-21 15:48:53 -0700

    fixed age

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let data = {
-    "TLDR": "I'm Ankkit, a 19 year old full stack developer. I'm currently studying at the University of British Columbia.",
-    "Details": "My name is Ankkit Prakash. I am a 19 year old full stack developer. I'm pursuing a Major in Computer Science and Minor in Mathematics at the University of British Columbia. ",
+    "TLDR": "I'm Ankkit, a 20 year old full stack developer. I'm currently studying at the University of British Columbia.",
+    "Details": "My name is Ankkit Prakash. I am a 20 year old full stack developer. I'm pursuing a Major in Computer Science and Minor in Mathematics at the University of British Columbia. ",
 }
 function aboutSwitcher(id) {
     let about = document.getElementById("about-text");
